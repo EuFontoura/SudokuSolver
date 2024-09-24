33b99053b88a4594c7df02ef6a88519b1e1c4ba5
EuFontoura <gabrielFMendonca@gmail.com>
2024-09-24 10:01:02 -0300

    Complete project: Sudoku solver implemented

---
+++ main.py
@@ -0,0 +1,25 @@
+from sudoku import solve_sudoku
+
+####################
+### EXEMPLO DE USO ###
+####################
+
+# -1 indica linhas vazias
+puzzle_exemplo = [
+    [5, 3, -1, -1, 7, -1, -1, -1, -1],
+    [6, -1, -1, 1, 9, 5, -1, -1, -1],
+    [-1, 9, 8, -1, -1, -1, -1, 6, -1],
+    [8, -1, -1, -1, 6, -1, -1, -1, 3],
+    [4, -1, -1, 8, -1, 3, -1, -1, 1],
+    [7, -1, -1, -1, 2, -1, -1, -1, 6],
+    [-1, 6, -1, -1, -1, -1, 2, 8, -1],
+    [-1, -1, -1, 4, 1, 9, -1, -1, 5],
+    [-1, -1, -1, -1, 8, -1, -1, 7, 9]
+]
+
+# Resolver o puzzle
+if solve_sudoku(puzzle_exemplo):
+    for row in puzzle_exemplo:
+        print(row)
+else:
+    print("Este quebra-cabeça não tem solução.")
